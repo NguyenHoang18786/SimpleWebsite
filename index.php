e034ae22a42a3c49acb2ab62b34a5cef3c6892bc
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-03 17:18:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,9 @@
 <?php
 echo "Hello Cloud Computing Class!<br>";
 echo "Nguyễn Hoàng";
-?>
-  
-<?php
+<br/>
 // connect to a database 
-$dbConn = pg_connect("host=ec2-54-158-1-189.compute-1.amazonaws.com
- port=5432 dbname=dm3thdq3v0u36 user=equifalumcnmkg password=7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786");
+$dbConn = pg_connect("host=ec2-54-158-1-189.compute-1.amazonaws.com port=5432 dbname=dm3thdq3v0u36 user=equifalumcnmkg password=7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786");
 if (!$dbConn) {
     echo "An error occurred.\n";
     exit;
@@ -22,6 +19,13 @@ if (!$result) {
     echo "An error occurred.\n";
     exit;
 }
+  
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>Product</th>
+<th>Price</th>
+</tr>";
 // Show value
 while ($row = pg_fetch_assoc($result)) {
 echo "<tr>";
@@ -30,13 +34,6 @@ echo "<td>" . $row['product_name'] . "</td>";
 echo "<td>" . $row['product_price'] . "</td>";
 echo "</tr>";
 }echo "</table>";
-
-echo "<table border='1'>
-<tr>
-<th>ID</th>
-<th>Product</th>
-<th>Price</th>
-</tr>";
 
 ?>
 </body>
