d6d20fa2d522667612e2386682a636146bf58e18
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-03 17:02:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,35 @@
 echo "Hello Cloud Computing Class!<br>";
 echo "Nguyễn Hoàng";
 ?>
+  
+<?php
+$con=mysqli_connect("ec2-54-158-1-189.compute-1.amazonaws.com","equifalumcnmkg","7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786","dm3thdq3v0u36");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
+$result = mysqli_query($con,"SELECT * FROM Persons");
+
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>Product</th>
+<th>Price</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['product_id'] . "</td>";
+echo "<td>" . $row['product_name'] . "</td>";
+echo "<td>" . $row['product_price'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
 </body>
 </html>
