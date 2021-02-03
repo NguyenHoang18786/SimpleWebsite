d68e86876f1acaf8d97538c57544bfead688ce94
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-03 17:23:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 <body>
-
   <center><h1>My first PHP page</h1></center>
-
 <?php
+  
 echo "Hello Cloud Computing Class!<br>";
 echo "Nguyễn Hoàng";
 <br/>
+  
 // connect to a database 
 $dbConn = pg_connect("host=ec2-54-158-1-189.compute-1.amazonaws.com port=5432 dbname=dm3thdq3v0u36 user=equifalumcnmkg password=7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786");
 if (!$dbConn) {
@@ -20,14 +20,14 @@ if (!$result) {
     exit;
 } 
 
-// Show value
-while ($row = pg_fetch_assoc($result)) {
 echo "<table border='1'>
 <tr>
 <th>ID</th>
 <th>Product</th>
 <th>Price</th>
 </tr>";
+// Show value
+while ($row = pg_fetch_assoc($result)) {
 echo "<tr>";
 echo "<td>" . $row['product_id'] . "</td>";
 echo "<td>" . $row['product_name'] . "</td>";
