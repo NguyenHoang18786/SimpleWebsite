b29eef916819a93d15deb68ba3c2ae91c5316d7f
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-03 17:13:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,27 @@ echo "Nguyễn Hoàng";
 ?>
   
 <?php
-$con=mysqli_connect("ec2-54-158-1-189.compute-1.amazonaws.com","equifalumcnmkg","7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786","dm3thdq3v0u36");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// connect to a database 
+$dbConn = pg_connect("host=<ec2-54-158-1-189.compute-1.amazonaws.com
+> port=<5432> dbname=<dm3thdq3v0u36> user=<equifalumcnmkg> password=<7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786>");
+if (!$dbConn) {
+    echo "An error occurred.\n";
+    exit;
 }
-
-$result = mysqli_query($con,"SELECT * FROM test_lab6");
+// Query data
+$result = pg_query($dbConn, 'SELECT * FROM test_lab6');
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+// Show value
+while ($row = pg_fetch_assoc($result)) {
+echo "<tr>";
+echo "<td>" . $row['product_id'] . "</td>";
+echo "<td>" . $row['product_name'] . "</td>";
+echo "<td>" . $row['product_price'] . "</td>";
+echo "</tr>";
+}echo "</table>";
 
 echo "<table border='1'>
 <tr>
@@ -25,17 +38,6 @@ echo "<table border='1'>
 <th>Price</th>
 </tr>";
 
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['product_id'] . "</td>";
-echo "<td>" . $row['product_name'] . "</td>";
-echo "<td>" . $row['product_price'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-
-mysqli_close($con);
 ?>
 </body>
 </html>
