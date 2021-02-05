2a4f657c8b91c01cf24cb879ff98e6a291fefd70
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-05 13:29:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,57 @@
 <html>
-<body>
-  <center><h1>My first PHP page</h1></center>
-<?php
-  
-echo "Hello Cloud Computing Class!<br>";
-echo "Nguyễn Hoàng";
-<br/>
-  
-// connect to a database 
-$dbConn = pg_connect("host=ec2-54-158-1-189.compute-1.amazonaws.com port=5432 dbname=dm3thdq3v0u36 user=equifalumcnmkg password=7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786");
-if (!$dbConn) {
-    echo "An error occurred.\n";
-    exit;
-}
-// Query data
-$result = pg_query($dbConn, 'SELECT * FROM test_lab6');
-if (!$result) {
-    echo "An error occurred.\n";
-    exit;
-} 
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php 
+			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
+			# Heroku credential 
+			$host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
+			$db_heroku = "dm3thdq3v0u36";
+			$user_heroku = "equifalumcnmkg";
+			$pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786";
+			# Create connection to Heroku Postgres
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+			# Get data by query
+			$query = 'select * from test_lab6';
+			$result = pg_query($pg_heroku, $query);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
 
-// Show value
-while ($row = pg_fetch_assoc($result)) {
-echo "<table border='1'>
-echo <tr>
-<th>ID</th>
-<th>Product</th>
-<th>Price</th>
-</tr>";
-echo "<tr>";
-echo "<td>" . $row['product_id'] . "</td>";
-echo "<td>" . $row['product_name'] . "</td>";
-echo "<td>" . $row['product_price'] . "</td>";
-echo "</tr>";
-} echo "</table>";
+			echo '</table></body></html>';
 
-?>
-</body>
+		?> 
+	</body>
 </html>
