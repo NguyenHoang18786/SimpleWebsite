9683e5fe60ee7b2d8012e383bea032d709d56fd7
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 04:17:06 +0700

    Update login.php

--- login.php
+++ login.php
@@ -15,6 +15,39 @@
   $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
   $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
   $pg_heroku = pg_connect($conn_string);
+  $query = 'select * from accounts';
+			$result = pg_query($pg_heroku, $query);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
+
+			echo '</table></body></html>';
+
     if(isset($_POST['submit'])&&!empty($_POST['submit'])){    
         $hashpassword = md5($_POST['password']);
         $sql ="select username, password from accounts where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
