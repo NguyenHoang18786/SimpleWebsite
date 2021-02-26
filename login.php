6132bf9428fc8d6c0ba0e2a05db1e85c8517adb7
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 02:46:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -8,48 +8,39 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php
-$host = "ec2-54-158-1-189.compute-1.amazonaws.com";
-$port = "5432";
-$dbname = "dm3thdq3v0u36";
-$user = "equifalumcnmkg";
-$password = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
-$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-$dbconn = pg_connect($connection_string);
-if(isset($_POST['submit'])&&!empty($_POST['submit'])){
-    
-    $hashpassword = md5($_POST['pwd']);
-    $sql ="select *from public.user where email = '".pg_escape_string($_POST['email'])."' and password ='".$hashpassword."'";
-    $data = pg_query($dbconn,$sql); 
-    $login_check = pg_num_rows($data);
-    if($login_check > 0){ 
-        
-        echo "Login Successfully";    
-    }else{
-        
-        echo "Invalid Details";
+  <?php
+  $host = "ec2-54-158-1-189.compute-1.amazonaws.com";
+  $port = "5432";
+  $dbname = "dm3thdq3v0u36";
+  $user = "equifalumcnmkg";
+  $password = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+  $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+  $dbconn = pg_connect($connection_string);
+    if(isset($_POST['submit'])&&!empty($_POST['submit'])){    
+        $hashpassword = md5($_POST['password']);
+        $sql ="select * from accounts where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
+        $data = pg_query($dbconn,$sql); 
+        $login_check = pg_num_rows($data);
+        if($login_check > 0){     
+            echo "Login Successfully";    
+        }else{        
+            echo "Invalid Details";
+        }
     }
-}
-?>
-<div class="container">
-  <h2>Login Here </h2>
-  <form method="post">
-  
-     
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    
-     
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-    </div>
-     
-    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
-  </form>
-</div>
-
+  ?>
+  <div class="container-lg">
+    <h2>Login for ATN storage </h2>
+    <form method="post">      
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="username" class="form-control" id="username" placeholder="Please enter username ..." name="username">
+      </div>     
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" class="form-control" id="password" placeholder="Please enter password ..." name="password">
+      </div>     
+      <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+    </form>
+  </div>
 </body>
 </html>
