ad805648332f2ae740637e6aa357aa6f4e70704c
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 03:40:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -10,12 +10,11 @@
 <body>
   <?php
   $host = "ec2-54-158-1-189.compute-1.amazonaws.com";
-  $port = "5432";
   $dbname = "dm3thdq3v0u36";
   $user = "equifalumcnmkg";
-  $password = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
-  $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-  $dbconn = pg_connect($connection_string);
+  $dbpassword = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+  $conn_string = "host=$host port=5432 dbname=$dbname user=$user password=$dbpassword";
+  $dbconn = pg_connect($conn_string);
     if(isset($_POST['submit'])&&!empty($_POST['submit'])){    
         $hashpassword = md5($_POST['password']);
         $sql ="select * from accounts where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
