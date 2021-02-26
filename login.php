787b6ed4447b3848233b38965354b03497946bfd
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 04:26:28 +0700

    Update login.php

--- login.php
+++ login.php
@@ -9,57 +9,36 @@
 </head>
 <body>
   <?php
+	session_start();
   $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
   $db_heroku = "dm3thdq3v0u36";
   $user_heroku = "equifalumcnmkg";
   $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
   $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
   $pg_heroku = pg_connect($conn_string);
-  $query = 'select * from accounts';
-			$result = pg_query($pg_heroku, $query);
-			# Display data column by column
-			$i = 0;
-			echo '<html><body><table><tr>';
-			while ($i < pg_num_fields($result))
-			{
-				$fieldName = pg_field_name($result, $i);
-				echo '<td>' . $fieldName . '</td>';
-				$i = $i + 1;
-			}
-			echo '</tr>';
-			# Display data row by row
-			$i = 0;
-			while ($row = pg_fetch_row($result)) 
-			{
-				echo '<tr>';
-				$count = count($row);
-				$y = 0;
-				while ($y < $count)
-				{
-					$c_row = current($row);
-					echo '<td>' . $c_row . '</td>';
-					next($row);
-					$y = $y + 1;
-				}
-				echo '</tr>';
-				$i = $i + 1;
-			}
-			pg_free_result($result);
-
-			echo '</table></body></html>';
-
-    if(isset($_POST['submit'])&&!empty($_POST['submit'])){    
-        $hashpassword = md5($_POST['password']);
-        $sql ="select username, password from accounts where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
-        $data = pg_query($pg_heroku, $sql); 
-        $login_check = pg_num_rows($data);
-        if($login_check > 0){     
-            header("Location: storage.php");
+	
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+        $sql ="select username, password from accounts where username = '$username' and password = md5('$password')";
+        $result = pg_query($pg_heroku, $sql); 
+        
+        if($result == false) {   
+	    echo "Invalid Details";           
             exit;    
-        }else{        
-            echo "Invalid Details";
-        }
-    }
+	$login_check = pg_num_rows($result);
+	if($login_check == 1) {
+	  $_SESSION["login"] = "OK";
+	  $_SESSION["username"] = $username;
+	 $redirect = "storage.php";
+	}
+	else
+	 $redirect = "login.php";
+
+	pg_free_result($result);
+	pg_close($sql);
+
+	header("Location: $redirect");
+
   ?>
   <center>
     <div class="container">
