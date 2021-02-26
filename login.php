775c6d2ad5ca57fbd09d0162839bd97f09dfa586
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 03:47:04 +0700

    Update login.php

--- login.php
+++ login.php
@@ -9,12 +9,12 @@
 </head>
 <body>
   <?php
-  $host = "ec2-54-158-1-189.compute-1.amazonaws.com";
-  $dbname = "dm3thdq3v0u36";
-  $user = "equifalumcnmkg";
-  $dbpassword = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
-  $conn_string = "host=$host port=5432 dbname=$dbname user=$user password=$dbpassword";
-  $dbconn = pg_connect($conn_string);
+  $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
+  $db_heroku = "dm3thdq3v0u36";
+  $user_heroku = "equifalumcnmkg";
+  $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+  $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+  $pg_heroku = pg_connect($conn_string);
   if (!$pg_heroku)
 			{
 				die('Error: Could not connect: ' . pg_last_error());
