4412bb8cbb1d5e6e6d8d6f2a0c83ac804ab4f2a2
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 05:32:33 +0700

    Update login.php

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if(isset($_POST['submit'])){
     if($login_check == 0){        
         echo "Invalid Details";   
     }else{        
-        header('location: index.php'); 
+        header('location: storage.php'); 
     }
 }
   ?>
