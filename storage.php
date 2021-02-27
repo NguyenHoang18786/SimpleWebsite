4fdb4b8c8dbf06a632931ed9d3f920e70e5d8d9f
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 09:50:57 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -25,7 +25,7 @@
 		</button>
 	      </div>
 		<form class="d-flex">
-		<input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
+		<input class="form-control me-2" style="margin-top: 10px margin-left: 200px" type="search" placeholder="Search..." aria-label="Search">
 		<button class="btn btn-outline-success" type="submit">Search</button>
 	      </form>
 	    </nav>
