a7920c98646adab4844a2aff2410966b53aad7b8
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-28 05:00:38 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -30,10 +30,11 @@ session_start();
 		<span class="icon-bar"></span>
 		</button>
 	      </div>
+		<div id="navbarCollapse" class="collapse navbar-collapse">
 		<form method="post" class="navbar-form form-inline>
 		<div class="input-group search-box" style="margin-left: 475px">
 		<input class="form-control me-2" type="search" name="search" id="search" placeholder="Search..." aria-label="Search">
-		<ul class="nav"><input type="submit" name="submit" class="btn btn-primary get-started-btn mt-1 mb-1" value="Submit"></ul>
+		<ul class="nav navbar-nav"><input type="submit" name="submit" class="btn btn-primary get-started-btn mt-1 mb-1" value="Submit"></ul>
 		</div>
 	      </form>
 	    <ul class="nav navbar-nav navbar-right">
@@ -41,6 +42,7 @@ session_start();
             <a href="StorageManagement.php" class="btn btn-outline-success" style="margin-left: 365px"> &nbsp;&nbsp;Storage Management</a>            
           </li>
            </ul>
+	</div>
 	    </nav>
 		<style>
 		table {
