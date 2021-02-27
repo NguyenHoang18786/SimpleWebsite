bddb566be465f4e33146a8189e7e7357afa262e8
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-28 02:49:24 +0700

    Update StorageManagement.php

--- StorageManagement.php
+++ StorageManagement.php
@@ -43,89 +43,32 @@
   </head>
 <body>
    <div class="container px-4">
-  <div class="row gx-5">
-    <div class="col">
-     <div class="p-3 border bg-light">
-       <center><h3>Add New Product</h3></center>
-         <form method="post">
-           <div class="form-group">
-             <label for="productid">Product ID</label>
-             <input type="text" class="form-control" id="productid" name="productid" aria-describedby="productid">
-           </div>
-           <div class="form-group">
-             <label for="productname">Product Name</label>
-             <input type="text" class="form-control" id="productname" name="productname">
-           </div>
-          <div class="form-group">
-             <label for="producttype">Product Type</label>
-             <input type="text" class="form-control" id="producttype" name="producttype">
-           </div>
-            <div class="form-group">
-             <label for="productprice">Product Price</label>
-             <input type="text" class="form-control" id="productprice" name="productprice">
-           </div>
-            <div class="form-group">
-             <label for="shopid">Shop ID</label>
-             <input type="text" class="form-control" id="shopid" name="shopid">
-           </div>
-            <input type="submit" name="submit" class="btn btn-outline-primary" value="Submit">
-        <?php
-           $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
-           $db_heroku = "dm3thdq3v0u36";
-           $user_heroku = "equifalumcnmkg";
-           $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
-           $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-           $pg_heroku = pg_connect($conn_string);
-
-                  $query1 = "select * from atnshop_storage";
-                  $result1 = pg_query($pg_heroku,$query1);
-                  $before_insert = pg_num_rows($result1);
-               if(isset($_POST['submit'])){
-                        $productid = $_POST["productid"];
-                        $productname = $_POST["productname"];
-                        $producttype = $_POST["producttype"];
-                        $productprice = $_POST["productprice"];
-                        $shopid = $_POST["shopid"];
-                  $query2 ="insert into atnshop_storage VALUES ('$productid','$productname','$producttype','$productprice','$shopid')";
-                  $result2 = pg_query($pg_heroku,$query2);
-                  $query3 = "select * from atnshop_storage";
-                  $result3 = pg_query($pg_heroku,$query3);
-                   $check_insert = pg_num_rows($result3);
-                 if($check_insert > $before_insert){        
-                    echo "Successfully Add New Product!!";   
-                }else{        
-                    echo "Something wrong, please input again"; 
-                }
-            }
-         ?>
-         </form>
-       </div>
-    </div>
+     <div class="row gx-5">
        <div class="col">
         <div class="p-3 border bg-light">
-          <center><h3>Update Product Informations</h3></center>
+          <center><h3>Add New Product</h3></center>
             <form method="post">
               <div class="form-group">
-                <label for="productidu">Product ID</label>
-                <input type="text" class="form-control" id="productidu" name="productidu" aria-describedby="productid">
+                <label for="productid">Product ID</label>
+                <input type="text" class="form-control" id="productid" name="productid" aria-describedby="productid">
               </div>
               <div class="form-group">
-                <label for="productnameu">Product Name</label>
-                <input type="text" class="form-control" id="productnameu" name="productnameu">
+                <label for="productname">Product Name</label>
+                <input type="text" class="form-control" id="productname" name="productname">
               </div>
              <div class="form-group">
-                <label for="producttypeu">Product Type</label>
-                <input type="text" class="form-control" id="producttypeu" name="producttypeu">
+                <label for="producttype">Product Type</label>
+                <input type="text" class="form-control" id="producttype" name="producttype">
               </div>
                <div class="form-group">
-                <label for="productpriceu">Product Price</label>
-                <input type="text" class="form-control" id="productpriceu" name="productpriceu">
+                <label for="productprice">Product Price</label>
+                <input type="text" class="form-control" id="productprice" name="productprice">
               </div>
                <div class="form-group">
-                <label for="shopidu">Shop ID</label>
-                <input type="text" class="form-control" id="shopidu" name="shopidu">
+                <label for="shopid">Shop ID</label>
+                <input type="text" class="form-control" id="shopid" name="shopid">
               </div>
-               <input type="submit" name="submitu" class="btn btn-outline-primary" value="Submit">
+               <input type="submit" name="submit" class="btn btn-outline-primary" value="Submit">
            <?php
               $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
               $db_heroku = "dm3thdq3v0u36";
@@ -134,88 +77,145 @@
               $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
               $pg_heroku = pg_connect($conn_string);
 
-                           $productidu = $_POST["productidu"];
-                           $productnameu = $_POST["productnameu"];
-                           $producttypeu = $_POST["producttypeu"];
-                           $productpriceu = $_POST["productpriceu"];
-                           $shopidu = $_POST["shopidu"];
-                     $query5 = "select * from atnshop_storage where product_id = '$productidu' ";
-                     $result5 = pg_query($pg_heroku,$query5);
-                     $row = pg_fetch_assoc($result5);
-                  if (isset($_POST['submitu'])) {
-                     $query6 = "update atnshop_storage set product_name = '$productnameu', product_type = '$producttypeu', product_price = '$productpriceu', shop_id = '$shopidu' where product_id = '$productidu' ";
-                     $result6 = pg_query($pg_heroku,$query6);
-                     $query7 = "select * from atnshop_storage where product_id = '$productidu' ";
-                     $result7 = pg_query($pg_heroku,$query7);
-                     $rownew = pg_fetch_assoc($result7);
-                        if ($rownew != $row) {
-                            echo "Update successfull!";
-                       }else{
-                            echo "Update failed!!";
-                            }
-                  }
+                     $query1 = "select * from atnshop_storage";
+                     $result1 = pg_query($pg_heroku,$query1);
+                     $before_insert = pg_num_rows($result1);
+                  if(isset($_POST['submit'])){
+                           $productid = $_POST["productid"];
+                           $productname = $_POST["productname"];
+                           $producttype = $_POST["producttype"];
+                           $productprice = $_POST["productprice"];
+                           $shopid = $_POST["shopid"];
+                     $query2 ="insert into atnshop_storage VALUES ('$productid','$productname','$producttype','$productprice','$shopid')";
+                     $result2 = pg_query($pg_heroku,$query2);
+                     $query3 = "select * from atnshop_storage";
+                     $result3 = pg_query($pg_heroku,$query3);
+                      $check_insert = pg_num_rows($result3);
+                    if($check_insert > $before_insert){        
+                       echo "Successfully Add New Product!!";   
+                   }else{        
+                       echo "Something wrong, please input again"; 
+                   }
+               }
             ?>
             </form>
           </div>
        </div>
-        <div class="col">
-             <div class="p-3 border bg-light">
-          <center><h3>Get Product</h3></center>
-            <form method="post">
-              <div class="form-group">
-                <label for="productidu">Product ID</label>
-                <input type="text" class="form-control" id="productidu" name="productidu" aria-describedby="productid">
-              </div>
-              <div class="form-group">
-                <label for="productnameu">Product Name</label>
-                <input type="text" class="form-control" id="productnameu" name="productnameu">
-              </div>
-             <div class="form-group">
-                <label for="producttypeu">Product Type</label>
-                <input type="text" class="form-control" id="producttypeu" name="producttypeu">
-              </div>
-               <div class="form-group">
-                <label for="productpriceu">Product Price</label>
-                <input type="text" class="form-control" id="productpriceu" name="productpriceu">
-              </div>
-               <div class="form-group">
-                <label for="shopidu">Shop ID</label>
-                <input type="text" class="form-control" id="shopidu" name="shopidu">
-              </div>
-               <input type="submit" name="submitu" class="btn btn-outline-primary" value="Submit">
-           <?php
-              $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
-              $db_heroku = "dm3thdq3v0u36";
-              $user_heroku = "equifalumcnmkg";
-              $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
-              $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-              $pg_heroku = pg_connect($conn_string);
+          <div class="col">
+           <div class="p-3 border bg-light">
+             <center><h3>Update Product Informations</h3></center>
+               <form method="post">
+                 <div class="form-group">
+                   <label for="productidu">Product ID</label>
+                   <input type="text" class="form-control" id="productidu" name="productidu" aria-describedby="productid">
+                 </div>
+                 <div class="form-group">
+                   <label for="productnameu">Product Name</label>
+                   <input type="text" class="form-control" id="productnameu" name="productnameu">
+                 </div>
+                <div class="form-group">
+                   <label for="producttypeu">Product Type</label>
+                   <input type="text" class="form-control" id="producttypeu" name="producttypeu">
+                 </div>
+                  <div class="form-group">
+                   <label for="productpriceu">Product Price</label>
+                   <input type="text" class="form-control" id="productpriceu" name="productpriceu">
+                 </div>
+                  <div class="form-group">
+                   <label for="shopidu">Shop ID</label>
+                   <input type="text" class="form-control" id="shopidu" name="shopidu">
+                 </div>
+                  <input type="submit" name="submitu" class="btn btn-outline-primary" value="Submit">
+                    <?php
+                       $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
+                       $db_heroku = "dm3thdq3v0u36";
+                       $user_heroku = "equifalumcnmkg";
+                       $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+                       $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+                       $pg_heroku = pg_connect($conn_string);
 
-                           $productidu = $_POST["productidu"];
-                           $productnameu = $_POST["productnameu"];
-                           $producttypeu = $_POST["producttypeu"];
-                           $productpriceu = $_POST["productpriceu"];
-                           $shopidu = $_POST["shopidu"];
-                     $query5 = "select * from atnshop_storage where product_id = '$productidu' ";
-                     $result5 = pg_query($pg_heroku,$query5);
-                     $row = pg_fetch_assoc($result5);
-                  if (isset($_POST['submitu'])) {
-                     $query6 = "update atnshop_storage set product_name = '$productnameu', product_type = '$producttypeu', product_price = '$productpriceu', shop_id = '$shopidu' where product_id = '$productidu' ";
-                     $result6 = pg_query($pg_heroku,$query6);
-                     $query7 = "select * from atnshop_storage where product_id = '$productidu' ";
-                     $result7 = pg_query($pg_heroku,$query7);
-                     $rownew = pg_fetch_assoc($result7);
-                        if ($rownew != $row) {
-                            echo "Update successfull!";
-                       }else{
-                            echo "Update failed!!";
-                            }
-                  }
-            ?>
-            </form>
-          </div>  
-       </div>
-  </div>
-</div>
+                                    $productidu = $_POST["productidu"];
+                                    $productnameu = $_POST["productnameu"];
+                                    $producttypeu = $_POST["producttypeu"];
+                                    $productpriceu = $_POST["productpriceu"];
+                                    $shopidu = $_POST["shopidu"];
+                              $query5 = "select * from atnshop_storage where product_id = '$productidu' ";
+                              $result5 = pg_query($pg_heroku,$query5);
+                              $row = pg_fetch_assoc($result5);
+                           if (isset($_POST['submitu'])) {
+                              $query6 = "update atnshop_storage set product_name = '$productnameu', product_type = '$producttypeu', product_price = '$productpriceu', shop_id = '$shopidu' where product_id = '$productidu' ";
+                              $result6 = pg_query($pg_heroku,$query6);
+                              $query7 = "select * from atnshop_storage where product_id = '$productidu' ";
+                              $result7 = pg_query($pg_heroku,$query7);
+                              $rownew = pg_fetch_assoc($result7);
+                                 if ($rownew != $row) {
+                                     echo "Update successfull!";
+                                }else{
+                                     echo "Update failed!!";
+                                     }
+                           }
+                     ?>
+               </form>
+             </div>
+          </div>
+           <div class="col">
+             <div class="p-3 border bg-light">
+                <center><h3>Get Product</h3></center>
+                  <form method="post">
+                    <div class="form-group">
+                      <label for="productidu">Product ID</label>
+                      <input type="text" class="form-control" id="productidu" name="productidu" aria-describedby="productid">
+                    </div>
+                    <div class="form-group">
+                      <label for="productnameu">Product Name</label>
+                      <input type="text" class="form-control" id="productnameu" name="productnameu">
+                    </div>
+                   <div class="form-group">
+                      <label for="producttypeu">Product Type</label>
+                      <input type="text" class="form-control" id="producttypeu" name="producttypeu">
+                    </div>
+                     <div class="form-group">
+                      <label for="productpriceu">Product Price</label>
+                      <input type="text" class="form-control" id="productpriceu" name="productpriceu">
+                    </div>
+                     <div class="form-group">
+                      <label for="shopidu">Shop ID</label>
+                      <input type="text" class="form-control" id="shopidu" name="shopidu">
+                    </div>
+                     <input type="submit" name="submitu" class="btn btn-outline-primary" value="Submit">
+                    <?php
+                       $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
+                       $db_heroku = "dm3thdq3v0u36";
+                       $user_heroku = "equifalumcnmkg";
+                       $pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+                       $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+                       $pg_heroku = pg_connect($conn_string);
+
+                                    $productidu = $_POST["productidu"];
+                                    $productnameu = $_POST["productnameu"];
+                                    $producttypeu = $_POST["producttypeu"];
+                                    $productpriceu = $_POST["productpriceu"];
+                                    $shopidu = $_POST["shopidu"];
+                              $query5 = "select * from atnshop_storage where product_id = '$productidu' ";
+                              $result5 = pg_query($pg_heroku,$query5);
+                              $row = pg_fetch_assoc($result5);
+                           if (isset($_POST['submitu'])) {
+                              $query6 = "update atnshop_storage set product_name = '$productnameu', product_type = '$producttypeu', product_price = '$productpriceu', shop_id = '$shopidu' where product_id = '$productidu' ";
+                              $result6 = pg_query($pg_heroku,$query6);
+                              $query7 = "select * from atnshop_storage where product_id = '$productidu' ";
+                              $result7 = pg_query($pg_heroku,$query7);
+                              $rownew = pg_fetch_assoc($result7);
+                                 if ($rownew != $row) {
+                                     echo "Update successfull!";
+                                }else{
+                                     echo "Update failed!!";
+                                     }
+                           }
+                     ?>
+                   </form>
+                </div>  
+             </div>
+         </div>
+      </div>
    </body>
 </html>
