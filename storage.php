ca36b33f494edbc3034d63b12d68df7fca53d41c
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 07:23:23 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -1,6 +1,30 @@
 <html>
 	<head>
 		<title>ATN shop storages</title>
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script>
+      // Prevent dropdown menu from closing when click inside the form
+      $(document).on("click", ".navbar-right .dropdown-menu", function(e){
+        e.stopPropagation();
+      });
+    </script>
+	     <nav class="navbar navbar-default navbar-expand-lg navbar-light">
+	      <div class="navbar-header">
+		<a class="navbar-brand" href="index.php">ATN<b>Shop</b></a>  		
+		<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+		<span class="navbar-toggler-icon"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+	      </div>
+	    </nav>
 	</head>
 	<body>
 		<?php 
