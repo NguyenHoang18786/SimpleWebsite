9e9aa2604c43ed47074335da007f4e2b13dd3352
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 13:05:40 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -37,7 +37,7 @@ session_start();
 	      </form>
 	    <ul class="nav navbar-nav navbar-right">
            <li>
-            <a href="StorageManagement.php" class="btn btn-outline-success" style="margin-left: 400px"> Storage Management</a>            
+            <a href="StorageManagement.php" class="btn btn-outline-success" style="margin-left: 3750px"> Storage Management</a>            
           </li>
            </ul>
 	    </nav>
