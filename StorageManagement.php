48a17ba67fd99f8877b90c51b7c45a2310611416
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-28 03:26:21 +0700

    Update StorageManagement.php

--- StorageManagement.php
+++ StorageManagement.php
@@ -160,38 +160,33 @@
                   </form>
             </div>
           </div>
-        </div>
-      </div>   
-      <hr>
-      <div class="container px-4">
-        <div class="row gx-5">
-           <div class="col">
+                      <div class="col">
             <div class="p-3 border bg-light">
               <center>
-                     <h3>Get Product</h3>
+                     <h3>Delete Product</h3>
               </center>
                   <form method="post">
                      <div class="form-group">
-                        <label for="productidu">Product ID</label>
-                        <input type="text" class="form-control" id="productidu" name="productidu" aria-describedby="productid">
+                        <label for="productidd">Product ID</label>
+                        <input type="text" class="form-control" id="productidd" name="productidd" aria-describedby="productid">
                      </div>
                      <div class="form-group">
-                        <label for="productnameu">Product Name</label>
-                        <input type="text" class="form-control" id="productnameu" name="productnameu">
+                        <label for="productnamed">Product Name</label>
+                        <input type="text" class="form-control" id="productnamed" name="productnamed">
                      </div>
                      <div class="form-group">
-                        <label for="producttypeu">Product Type</label>
-                        <input type="text" class="form-control" id="producttypeu" name="producttypeu">
+                        <label for="producttyped">Product Type</label>
+                        <input type="text" class="form-control" id="producttyped" name="producttyped">
                      </div>
                      <div class="form-group">
-                        <label for="productpriceu">Product Price</label>
-                        <input type="text" class="form-control" id="productpriceu" name="productpriceu">
+                        <label for="productpriced">Product Price</label>
+                        <input type="text" class="form-control" id="productpriced" name="productpriced">
                      </div>
                      <div class="form-group">
-                        <label for="shopidu">Shop ID</label>
-                        <input type="text" class="form-control" id="shopidu" name="shopidu">
+                        <label for="shopidd">Shop ID</label>
+                        <input type="text" class="form-control" id="shopidd" name="shopidd">
                      </div>
-                     <input type="submit" name="submitu" class="btn btn-outline-primary" value="Submit">
+                     <input type="submit" name="submitd" class="btn btn-outline-primary" value="Submit">
                         <?php
                            $host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
                            $db_heroku = "dm3thdq3v0u36";
@@ -200,21 +195,21 @@
                            $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
                            $pg_heroku = pg_connect($conn_string);
 
-                                        $productidu = $_POST["productidu"];
-                                        $productnameu = $_POST["productnameu"];
-                                        $producttypeu = $_POST["producttypeu"];
-                                        $productpriceu = $_POST["productpriceu"];
-                                        $shopidu = $_POST["shopidu"];
-                                  $query5 = "select * from atnshop_storage where product_id = '$productidu' ";
-                                  $result5 = pg_query($pg_heroku,$query5);
-                                  $row = pg_fetch_assoc($result5);
-                               if (isset($_POST['submitu'])) {
-                                  $query6 = "update atnshop_storage set product_name = '$productnameu', product_type = '$producttypeu', product_price = '$productpriceu', shop_id = '$shopidu' where product_id = '$productidu' ";
-                                  $result6 = pg_query($pg_heroku,$query6);
-                                  $query7 = "select * from atnshop_storage where product_id = '$productidu' ";
-                                  $result7 = pg_query($pg_heroku,$query7);
-                                  $rownew = pg_fetch_assoc($result7);
-                                     if ($rownew != $row) {
+                                        $productidd = $_POST["productidd"];
+                                        $productnamed = $_POST["productnamed"];
+                                        $producttyped = $_POST["producttyped"];
+                                        $productpriced = $_POST["productpriced"];
+                                        $shopidd = $_POST["shopidd"];
+                                  $query8 = "select * from atnshop_storage";
+                                  $result8 = pg_query($pg_heroku,$query8);
+                                  $before_delete = pg_num_rows($result8);
+                               if (isset($_POST['submitd'])) {
+                                  $query9 = "delete from atnshop_storage where product_id = '$productidd' ";
+                                  $result9 = pg_query($pg_heroku,$query9);
+                                  $query10 = "select * from atnshop_storage";
+                                  $result10 = pg_query($pg_heroku,$query10);
+                                  $after_delete = pg_num_rows($result10);
+                                     if ($before_delete < $after_delete) {
                                          echo "Update successfull!";
                                     }else{
                                          echo "Update failed!!";
@@ -225,16 +220,13 @@
             </div>
           </div>
         </div>
-      </div>
+      </div>   
    </body>
-   <footer id="site-footer">
-    <div class="container-xl">
-       <div id="footer2">
-    <center>
-      <a class="linkFooter" href="ATNCompany@gmail.com">ATNCompany@gmail.com</a><br>
-      <p>Copyright  2020 Domain Name - All Rights Reserved </p>
-    </center>
-       </div>
-      </div>
-  </footer>
+<footer class="bg-light text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <a class="text-dark" href="ATNCompany@gmail.com">ATNCompany@gmail.com</a><br>
+    <p>Copyright  2020 Domain Name - All Rights Reserved </p>
+  </div>
+</footer>
 </html>
