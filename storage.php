57e733faa3d4cf40ba4c21efb4ce9a9a9217dda3
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-28 04:46:56 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -33,12 +33,12 @@ session_start();
 		<form method="post" class="navbar-form form-inline>
 		<div class="input-group search-box" style="margin-left: 475px">
 			<input class="form-control me-2" type="search" name="search" id="search" placeholder="Search..." aria-label="Search">
-		</div>
 		 <ul class="nav navbar-nav navbar-right">
 		 	<li>
 			<input type="submit" name="submit" class="btn btn-primary get-started-btn mt-1 mb-1" value="Submit">
 			</li>
-		</ul>
+		 </ul>
+		</div>
 	      </form>
 	    <ul class="nav navbar-nav navbar-right">
            <li>
