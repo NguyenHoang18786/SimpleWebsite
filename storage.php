c88dcc95ea732af9793a3c3f7b72333179c8d526
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 10:23:59 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -24,11 +24,57 @@
 		<span class="icon-bar"></span>
 		</button>
 	      </div>
-		<form class="navbar-form form-inline">
+		<form class="navbar-form form-inline method="post">
 			<div class="input-group search-box" style="margin-left: 500px">
-		<input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
+		<?php 
+			# Heroku credential 	 
+			$host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
+			$db_heroku = "dm3thdq3v0u36";
+			$user_heroku = "equifalumcnmkg";
+			$pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+			if(isset($_POST['submit']))
+			$query5 = "select * from atnshop_storage where product_id or product_name or product_price = $search";
+			$result = pg_query($pg_heroku, $query5);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
+			echo '</table></body></html>';
+		?> 
+		<input class="form-control me-2" type="search" name="search" placeholder="Search..." aria-label="Search">
 			</div>
-		<button class="btn btn-outline-success" type="submit">Search</button>
+		<button class="btn btn-outline-success" type="submit" name="submit">Search</button>
 	      </form>
 	    </nav>
 		<style>
