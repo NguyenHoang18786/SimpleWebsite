f0bb740252e3eefe93fbd97bc812fa1f3d79f60b
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 09:08:32 +0700

    Update storage.php

--- storage.php
+++ storage.php
@@ -143,5 +143,54 @@
 	    </div>
 	  </div>
 	</div>
+	 <div class="container-sm">
+		 <center><h3>Current price list of products of ATN shop</h3></center>
+		<?php 
+		# Heroku credential 	 
+		$host_heroku = "ec2-54-158-1-189.compute-1.amazonaws.com";
+		$db_heroku = "dm3thdq3v0u36";
+		$user_heroku = "equifalumcnmkg";
+		$pw_heroku = "7bbc29b6da39382b5f7a0fb0aa5a4bc737cd1174714f757097fbd2a4b0b87786"; 
+		$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+		$pg_heroku = pg_connect($conn_string);
+
+		if (!$pg_heroku)
+		{
+			die('Error: Could not connect: ' . pg_last_error());
+		}
+
+		$query3 = "select product_name, product_price from atnshop_storage group by product_name, product_price order by product_price asc";
+		$result = pg_query($pg_heroku, $query3);
+		# Display data column by column
+		$i = 0;
+		echo '<html><body><table><tr>';
+		while ($i < pg_num_fields($result))
+		{
+			$fieldName = pg_field_name($result, $i);
+			echo '<td>' . $fieldName . '</td>';
+			$i = $i + 1;
+		}
+		echo '</tr>';
+		# Display data row by row
+		$i = 0;
+		while ($row = pg_fetch_row($result)) 
+		{
+			echo '<tr>';
+			$count = count($row);
+			$y = 0;
+			while ($y < $count)
+			{
+				$c_row = current($row);
+				echo '<td>' . $c_row . '</td>';
+				next($row);
+				$y = $y + 1;
+			}
+			echo '</tr>';
+			$i = $i + 1;
+		}
+		pg_free_result($result);
+		echo '</table></body></html>';
+		?> 
+	 </div>
  </body>
 </html>
