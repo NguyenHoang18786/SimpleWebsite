9a172527af5d2b203fae8e847b03a28e4a7efa51
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-27 07:22:53 +0700

    Update login.php

--- login.php
+++ login.php
@@ -6,9 +6,19 @@ session_start();
 <head>
   <title>Login</title>
   <meta name="keywords" content="PHP,PostgreSQL,Insert,Login">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>     
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script>
+      // Prevent dropdown menu from closing when click inside the form
+      $(document).on("click", ".navbar-right .dropdown-menu", function(e){
+        e.stopPropagation();
+      });
+    </script>
     <nav class="navbar navbar-default navbar-expand-lg navbar-light">
       <div class="navbar-header">
         <a class="navbar-brand" href="index.php">ATN<b>Shop</b></a>  		
